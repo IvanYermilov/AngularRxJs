b6ce41e872f41f9496ade9d6f0a3229bd0d7f57b
User <User@gmail.com>
2023-02-22 14:52:43 +0300

    Implemented the third component

--- src/app/third.component.ts
+++ src/app/third.component.ts
@@ -1,7 +1,28 @@
 import { Component} from '@angular/core';
+import { interval, take, map, combineLatest, forkJoin, zip } from 'rxjs';
+
+const source1 = interval(200).pipe(take(10),map((val) => 'source1 val = ' + val));
+const source2 = interval(300).pipe(take(10),map((val) => 'source2 val = ' + val));
+const source3 = interval(400).pipe(take(10),map((val) => 'source3 val = ' + val));
   
 @Component({
     selector: 'third-comp',
-    template: `<h3>The third component</h3>`
+    template: `<h3>The third component</h3>
+    <button (click) = "firstTask()">Combine</button>
+    <button (click) = "secondTask()">Last values</button>
+    <button (click) = "thirdTask()">Wait each latest</button>`,
+    styles: ['button + button { margin-left: 10px;}']
 })
-export class ThirdComponent { }
+export class ThirdComponent {
+    firstTask(){
+        combineLatest([source1, source2, source3]).subscribe(val => console.log(val));
+    }
+
+    secondTask(){
+        forkJoin([source1, source2, source3]).subscribe(val => console.log(val));
+    }
+
+    thirdTask(){
+        zip(source1, source2, source3).subscribe(val => console.log(val));
+    }
+}
